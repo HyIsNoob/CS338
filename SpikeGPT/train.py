e2e5477ecdc082f15c1f8b822a44d48557ba18cc
HyIsNoob <90138555+HyIsNoob@users.noreply.github.com>
2026-05-09 08:36:40 +0700

    Update train.py

--- SpikeGPT/train.py
+++ SpikeGPT/train.py
@@ -103,16 +103,16 @@ if __name__ == '__main__':
     )
     if os.path.isfile(pretrained):
         print("\n" + "="*50)
-        print(f"✅ ĐÃ TÌM THẤY FILE PRE-TRAINED TẠI: {pretrained}")
+        print(f"ĐÃ TÌM THẤY FILE PRE-TRAINED TẠI: {pretrained}")
         m2 = torch.load(pretrained, map_location=torch.device("cpu"))
         load_result = model.load_state_dict(m2, strict=False)
-        print(f"✅ NẠP THÀNH CÔNG! Các layer chưa có tạ (sẽ khởi tạo random):")
+        print(f"NẠP THÀNH CÔNG! Các layer chưa có tạ (sẽ khởi tạo random):")
         print(load_result.missing_keys) # Sẽ in ra head_q và head_k
         print("="*50 + "\n")
     else:
         print("\n" + "!"*50)
-        print(f"❌ CẢNH BÁO: KHÔNG TÌM THẤY FILE {pretrained} !!!")
-        print("❌ QUÁ TRÌNH TRAIN SẼ BẮT ĐẦU VỚI RANDOM WEIGHTS (FROM SCRATCH)!!!")
+        print(f"CẢNH BÁO: KHÔNG TÌM THẤY FILE {pretrained} !!!")
+        print("QUÁ TRÌNH TRAIN SẼ BẮT ĐẦU VỚI RANDOM WEIGHTS (FROM SCRATCH)!!!")
         print("!"*50 + "\n")
 
 
