73c6f976b48c083fe655bafedee0a09b67f2dab2
HyIsNoob <90138555+HyIsNoob@users.noreply.github.com>
2026-05-10 09:05:21 +0700

    Update train.py

--- SpikeGPT/train.py
+++ SpikeGPT/train.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
 
     pretrained = os.environ.get(
         "SPIKEGPT_CHECKPOINT",
-        "/kaggle/input/models/hykhangg/spikegpt216m/pytorch/default/1/SpikeGPT-216M.pth",
+        "/kaggle/input/models/nviethoang/spikegpt-finetune-first12/pytorch/default/1/FineTune(best).pth",
     )
     if os.path.isfile(pretrained):
         print("\n" + "="*50)
